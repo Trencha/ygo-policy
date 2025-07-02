a9f378921cbaa14428db0a94e5b8071ccf3a190f
Trencha <t13lockett@googlemail.com>
2025-07-02 18:09:13 +0100

    Fix a few more bullet points

--- fix_bad_bullet_points.py
+++ fix_bad_bullet_points.py
@@ -5,14 +5,13 @@ for f in os.listdir():
         with open(f, encoding="utf-8") as old_file:
             old_file_content = old_file.readlines()
         new_file_content = []
-        add_bullet_next_line = False
+        prepend_next_line = ""
         for line in old_file_content:
-            if line == "•\n":
-                add_bullet_next_line = True
+            if line == "•\n" or line == "o\n":
+                prepend_next_line = line.replace("\n", " ")
             else:
-                if add_bullet_next_line:
-                    line = "• " + line
-                    add_bullet_next_line = False
+                line = prepend_next_line + line
+                prepend_next_line = ""
                 new_file_content.append(line)
         with open(f, "w", encoding="utf-8") as new_file:
             new_file.writelines(new_file_content)
